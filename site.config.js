99be9f00b97f0286a09f0826726ecd0fe4fa498d
bvalette <bvalette@student.42.fr>
2024-02-08 19:01:43 +0100

    init config

--- site.config.js
+++ site.config.js
@@ -1,25 +1,25 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
-    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
+    name: "name_first_line",
+    image: "/avatar.png",
+    role: "fullstack software engineer",
+    bio: "I am on a perpetual journey back to entrepreneurship",
+    email: "brian.valette@gmail.com",
+    linkedin: "brian-valette",
+    github: "brian-xu-vlt",
     instagram: "",
   },
   projects: [
     {
       name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
+      href: "https://github.com/brian-xu-vlt/notion-to-blog",
     },
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "Brian",
+    description: "I am still entrepreneuring.",
   },
 
   // CONFIG configration (required)
@@ -53,7 +53,7 @@ const CONFIG = {
     },
   },
   utterances: {
-    enable: true,
+    enable: false,
     config: {
       repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO || "",
       "issue-term": "og:title",
